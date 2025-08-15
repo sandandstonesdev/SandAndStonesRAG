044382236ed3c8a2d5c0045e535a1869d20cce48
Pawel <aksiutowicz.pawel@gmail.com>
2025-08-15 10:23:15 +0200

    Fullstack Config, Dockerization, Networking

---
+++ ConversationService/CorsConfig.cs
@@ -0,0 +1,10 @@
+﻿namespace ConversationService
+{
+    public class CorsConfig
+    {
+        public string[] AllowedOrigins { get; set; } = Array.Empty<string>();
+        public string[] AllowedHeaders { get; set; } = Array.Empty<string>();
+        public string[] AllowedMethods { get; set; } = Array.Empty<string>();
+        public bool AllowCredentials { get; set; }
+    }
+}
--- ConversationService/Program.cs
+++ ConversationService/Program.cs
@@ -1,4 +1,6 @@
+using Microsoft.AspNetCore.Cors.Infrastructure;
 using Microsoft.SemanticKernel;
+using ConversationService;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,20 +10,35 @@ var azureOpenAIKey = configuration["OpenAISecrets:ApiKey"];
 var azureOpenAIEndpoint = "https://proj-sand-and-stones-ra-resource.openai.azure.com/";
 var azureOpenAIDeploymentName = "gpt-4o-mini";
 
+builder.Services.Configure<CorsConfig>(builder.Configuration.GetSection("Cors"));
+
 builder.Services.AddAzureOpenAIChatCompletion(
                         deploymentName: azureOpenAIDeploymentName,
                         endpoint: azureOpenAIEndpoint,
                         apiKey: azureOpenAIKey);
 
+
 builder.Services.AddCors(options =>
 {
-    options.AddDefaultPolicy(builder =>
+    options.AddDefaultPolicy(policyBuilder =>
     {
-        builder
-            .WithOrigins("http://localhost:3001") // Or "*", for testing only
-            .AllowAnyHeader()
-            .AllowAnyMethod()
-            .AllowCredentials();
+        var corsOptions = configuration.GetSection("Cors").Get<CorsConfig>() ?? new CorsConfig();
+
+        _ = corsOptions.AllowedOrigins.Length > 0 ?
+            policyBuilder.WithOrigins(corsOptions.AllowedOrigins) :
+            policyBuilder.AllowAnyOrigin();
+
+        _ = corsOptions.AllowedHeaders.Length > 0 && corsOptions.AllowedHeaders[0] != "*" ?
+            policyBuilder.WithHeaders(corsOptions.AllowedHeaders) :
+            policyBuilder.AllowAnyHeader();
+
+        _ = corsOptions.AllowedMethods.Length > 0 && corsOptions.AllowedMethods[0] != "*" ?
+            policyBuilder.WithMethods(corsOptions.AllowedMethods) :
+            policyBuilder.AllowAnyMethod();
+
+        _ = corsOptions.AllowCredentials ?
+            policyBuilder.AllowCredentials() :
+            policyBuilder.DisallowCredentials();
     });
 });
 
@@ -34,7 +51,7 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
-//app.UseHttpsRedirection();
+app.UseHttpsRedirection();
 
 app.UseCors();
 
